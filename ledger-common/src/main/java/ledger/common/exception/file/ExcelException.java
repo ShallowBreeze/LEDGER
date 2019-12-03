44879deee16b1716c6db2e5e0110b9f950698157
pengzhen <pengzhen@cmhit.com>
2019-12-03 11:16:32 +0800

    首次提交，新建common

---
+++ ledger-common/src/main/java/ledger/common/exception/file/ExcelException.java
@@ -0,0 +1,12 @@
+package ledger.common.exception.file;
+
+
+import ledger.common.exception.base.BaseException;
+
+public class ExcelException extends BaseException {
+	private static final long serialVersionUID = 1L;
+
+	public ExcelException(String code, Object[] args) {
+		super("excel", code, args, null);
+	}
+}
